9d7bbe99a126039b5011f4cbe328ac420db44d79
sunjangyo12 <sunjangyo12@gmail.com>
2020-12-04 21:14:29 +0700

    fix download other apk

--- download.php
+++ download.php
@@ -33,8 +33,9 @@
             header('Content-Type: application/vnd.android.package-archive');
         }
         else {
-            header('Content-Length: '. filesize($dfile));
+            header('Content-Type: ' . finfo_file($finfo, $dfile));
         }
+        header('Content-Length: '. filesize($dfile));
         header(sprintf('Content-Disposition: attachment; filename=%s', strpos('MSIE',$_SERVER['HTTP_REFERER']) ? rawurlencode($filename) : "\"$filename\"" ));
         ob_flush();
         readfile($dfile);
