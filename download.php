79817c537f51db53d2664e9b207cad9ac9ed65e6
sunjangyo12 <sunjangyo12@gmail.com>
2020-12-04 20:40:50 +0700

    fix detect apk file download

--- download.php
+++ download.php
@@ -25,10 +25,16 @@
         exit();
     }
     else {
+        $mime = strtolower(pathinfo($dfile, PATHINFO_EXTENSION));
         $filename = basename($dfile);
         $finfo = finfo_open(FILEINFO_MIME_TYPE);
-        header('Content-Type: ' . finfo_file($finfo, $dfile));
-        header('Content-Length: '. filesize($dfile));
+
+        if ($mime == "apk") {
+            header('Content-Type: application/vnd.android.package-archive');
+        }
+        else {
+            header('Content-Length: '. filesize($dfile));
+        }
         header(sprintf('Content-Disposition: attachment; filename=%s', strpos('MSIE',$_SERVER['HTTP_REFERER']) ? rawurlencode($filename) : "\"$filename\"" ));
         ob_flush();
         readfile($dfile);
